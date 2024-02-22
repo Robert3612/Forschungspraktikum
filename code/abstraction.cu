1364389c8764ac1c08b2eb011b42762bffb4d574
Robert Schmidt <robert-schmidt10112000@outlook.de>
2024-02-22 12:40:33 +0000

    Treffen 22.02.

---
+++ code/abstraction.cu
@@ -0,0 +1,19 @@
+load data.txt -zero;
+> loaded data from file as 0
+> saved x byte
+
+load data2.txt -zero
+> loaded data from file as 1
+> saved x byte
+
+read 5 0;
+> value in 5 in 0 is y
+
+write 5 0 z;
+> value in 5 in 0 is now z
+
+add 0 1;
+> added 0 and 1
+
+store data.txt
+> stored in data.txt
--- code/zero-suppression.cu
+++ code/zero-suppression.cu
@@ -2,7 +2,6 @@
 #include <vector>
 #include <bitset>
 #include <algorithm>
-
 template <typename T>
 using EnableIfIntegral = std::enable_if_t<std::is_integral<T>::value>;
 
@@ -14,6 +13,15 @@ private:
     std::vector<int> valueBits;
 
 public:
+
+    std::vector<int> getValueBits(){
+        return valueBits;
+    }
+    
+    std::bitset<T> getBitset() {
+        return bitset;
+    }
+    
     template <size_t E>
     static std::vector<Slab<E>> packBitsets(const std::vector<std::string> &values)
     {
@@ -38,6 +46,7 @@ public:
             {
                 slabs.push_back(slab);
                 slab = Slab<E>();
+                bit_position = 0;
             }
 
             for (auto &ch : s)
@@ -82,6 +91,73 @@ public:
     }
 };
 
+class GPU_Slab
+{
+private:
+    bool* bitset;
+    int bitset_length;
+    int* valueBits;
+    int valueBits_length;
+public:
+
+    GPU_Slab(std::string CPU_bitset, std::vector<int> CPU_valueBits){
+        bitset = (bool*)malloc(CPU_bitset.size() * sizeof(bool));
+        for(int i=0;i<CPU_bitset.size();i++){
+            if(CPU_bitset[i]=='1'){
+                bitset[i] = true;
+            }
+            else{
+                bitset[i] = false;
+            }
+        }
+        bitset_length = CPU_bitset.size();
+
+        valueBits = (int*)malloc(CPU_valueBits.size() * sizeof(int));
+        for(int i=0;i<CPU_valueBits.size();i++){
+            valueBits[i] = CPU_valueBits[i];
+        }
+        valueBits_length = CPU_valueBits.size();
+    }
+
+    __host__ __device__ int* getValueBits(){
+        return valueBits;
+    }
+    
+    __host__ __device__ bool* getBitset() {
+        return bitset;
+    }
+
+    __host__ __device__ int getValueBits_length(){
+        return valueBits_length;
+    }
+    
+    __host__ __device__ int getBitset_length() {
+        return bitset_length;
+    }
+};
+
+__global__ 
+void add(GPU_Slab *slabs, int elementcount){
+
+    for(int i=0;i<elementcount;i++){
+
+        printf( "%d,", (slabs[i].getValueBits_length()));
+    for (int j=0;j< slabs[i].getValueBits_length();j++){
+        printf( "%d,", (slabs[i].getValueBits())[j]);
+    }
+
+    /**  
+    auto bitset = slab.getBitset();
+    for(int i=0;i<32;i++)
+        printf( "%d,", bitset[i])
+    printf( "\n")**/
+    }
+    
+    
+}
+
+
+
 void removeLeadingZeros(std::vector<std::string> &vector)
 {
 
@@ -93,17 +169,53 @@ void removeLeadingZeros(std::vector<std::string> &vector)
     }
 }
 
+template <size_t E>
+GPU_Slab* change(std::vector<Slab<E>> CPU_Slab){
+    GPU_Slab* r = (GPU_Slab*)malloc( CPU_Slab.size()* sizeof(GPU_Slab));
+    for(int i=0;i<CPU_Slab.size();i++){
+        r[i] = GPU_Slab(CPU_Slab[i].getBitset().to_string(), CPU_Slab[i].getValueBits());
+    }
+    return r;
+}
+
 int main()
 {
     // 2465,417
-    std::vector<std::string> h{"100110100001", "000110100001"};
+    std::vector<std::string> h{"100110100001", "000110100001","100000000000001","000100000001010101010101", "10101", "1010"};
 
     removeLeadingZeros(h);
 
-    auto slabs = Slab<64>::packBitsets<64>(h);
+    auto slabs = Slab<32>::packBitsets<32>(h);
+    std::cout<< slabs.size() << std::endl;
 
-    auto slab = slabs.front();
+    GPU_Slab* gpu_slaps = change(slabs);
 
-    for (auto s : slab.unpack())
-        std::cout << s << std::endl;
+    for(int i=0;i<slabs.size();i++){
+        for (int j=0;j< gpu_slaps[i].getValueBits_length();j++){
+            printf( "%d,", (gpu_slaps[i].getValueBits())[j]);
+        }
+        printf( "\n");
+    }
+    printf( "\n");
+
+    GPU_Slab* d_slab;
+    cudaMalloc(&d_slab, slabs.size() * sizeof(GPU_Slab));
+    cudaMemcpy(d_slab, gpu_slaps, slabs.size() * sizeof(GPU_Slab), cudaMemcpyHostToDevice);
+
+    add<<<1, 1>>>(d_slab, slabs.size());
+    cudaDeviceReset();
+
+    //auto slab = slabs.front();
+    /**
+    for(auto slab: slabs){
+    for (auto s : slab.getValueBits())
+        std::cout << s << ",";
+    
+    std::cout<<std::endl;
+        
+    auto bitset = slab.getBitset();
+    for(int i=0;i<32;i++)
+        std::cout << bitset[i] << ",";
+    std::cout<<std::endl;
+    }**/
 }
