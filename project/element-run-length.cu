95be3389ebf1791770b03ae1edbf1fb918fd71ac
Robert Schmidt <robert-schmidt10112000@outlook.de>
2024-04-20 13:17:08 +0000

    funished run length encode function

--- project/element-run-length.cu
+++ project/element-run-length.cu
@@ -4,20 +4,19 @@
 #include <algorithm>
 #include <math.h>
 
-   __device__ int decode_int(int *array, int* mask, int i)
+   __device__ int decode_int(char *array, int* mask, int i, int h)
 {
     int helper;
     int length;
     int number_length;
     int answer = 0;
 
-
-    helper = mask[i*3];
-    int number = array[helper];
-    number_length = array[helper+1];
-    length = mask[i*3+1];
+    helper = mask[i*3]; //0
+    int number = array[helper]; //1
+    number_length = array[helper+1]; //1
+    length = mask[i*3+1]; //3
     
-    if(length == mask[i*3+2]){
+    if(length == mask[i*3+2]){// 3==1 
         for(int j=0;j< length;j++){
             answer = answer + number * pow(10,j);
         }
@@ -28,15 +27,13 @@
             break;
         }
     }
-        while(length > 0){
+        for(int k = length ; k> 0 ;k--){ //3
             for(int j=0;j<number_length;j++){
-                answer = answer + number * pow(10, length-1);
-                length--;
-                if(length < 0){
-                    break;
-                }
+                answer = answer + number * pow(10, k-1);
+                k--;
             }
-            if(length > 0){
+            if(k > 0){
+            //printf("hallo9 %u %u %u\n", i, h, helper);
             helper = helper +2;
             number = array[helper];
             number_length = array[helper+1];
@@ -51,21 +48,23 @@
 
 
 __global__ 
-void add(int *A, int *B, int *C, int *mask_A, int *mask_B, int elementcount) {
-    
+void add(char *A, char *B, int *C, int *mask_A, int *mask_B, int elementcount) {
+    //printf("hallo \n");
     int a;
     int b;
     for (int i = blockIdx.x * blockDim.x + threadIdx.x; 
          i < elementcount; 
          i += blockDim.x * gridDim.x)
     {
-        a = decode_int(A, mask_A, i);
-        b = decode_int(B, mask_B, i);
+        a = decode_int(A, mask_A, i, 0);
+        b = decode_int(B, mask_B, i, 1);
+
+        printf("hallo4 %u %u %u\n", i, a, b);
         if(a > b){
-            C[i] = 1;
+            C[i] = (int) 1;
         }
         else{
-            C[i] = 0;
+            C[i] = (int) 0;
         }
     }
     
@@ -73,160 +72,232 @@ void add(int *A, int *B, int *C, int *mask_A, int *mask_B, int elementcount) {
 
 
 __global__
-void hello_world(int *A, int *mask_A, int i)
-{int a = decode_int(A, mask_A, i);
+void hello_world(char *A, int *mask_A, int i)
+{int a = decode_int(A, mask_A, i, 0);
  printf("Hello World From GPU!\n");
  printf ("answer: %d \n", a);
 }
 
-void encode(std::vector<int> start, std::vector<int> &mask, std::string &outcome){
-    std::string input = std::to_string(start.at(0));
-    std::vector<int> length;
-    length.push_back(std::to_string(start.at(0)).length());
-    std::vector<int> pos;
-    pos.push_back(std::to_string(start.at(0)).size()-1);
-    for(int i=1;i<start.size();i++){
-        input = input + std::to_string(start.at(i));
-        length.push_back(std::to_string(start.at(i)).length());
-        pos.push_back(pos.back() + std::to_string(start.at(i)).length());
-    }
-    
-    char letter = input[0];
-    int apperance = 1;
-    int length_index = 0;
-    int length_helper = length.at(length_index);
-    int pos_index = 0;
-    mask.push_back(0);
-    mask.push_back(length_helper);
-    bool again = false;
-    for(int i=1;i<input.length();i++){
-        if(letter != input[i]){
-            length_helper = length_helper - apperance;
-            while(length_helper < 0){
-                if(again){
-                    mask.push_back(length.at(length_index) +100);
-                }
-                length_index++;
-                mask.push_back(outcome.length());
-                mask.push_back(length.at(length_index));
-                length_helper = length_helper + length.at(length_index);
-                again = true;
-            }
-            again = false;
-            outcome = outcome + letter + std::to_string(apperance);
-            letter = input[i];
-            apperance = 1;
-            if(pos.at(pos_index)==i){
-                    mask.push_back(apperance +100);
-                    pos_index++;
-            }
+std::vector<uint64_t> compare_cpu(std::vector<uint64_t> a, std::vector<uint64_t> b){
+    std::vector<uint64_t> C;
+    for(int i=0;i<a.size();i++){
+        std::cout<<i<<", " << a.at(i)<<", "<<b.at(i)<<std::endl;
+        if(a.at(i) > b.at(i)){
+            C.push_back(1);
         }
         else{
-            apperance++;
-            //std::cout<< i<< std::endl;
-            //std::cout<< pos.at(pos_index)<< std::endl;
-            if(pos.at(pos_index)==i){
-                if(length.at(pos_index) > apperance){
-                    mask.push_back(apperance+100);
-                }
-                pos_index++;
-            }
+            C.push_back(0);
         }
     }
-            std::cout<< length_helper<<std::endl;
-            std::cout<< apperance<<std::endl;
-            length_helper = length_helper - apperance;
-            while(length_helper < 0){
-                if(again){
-                    mask.push_back(length.at(length_index) +100);
-                }
-                length_index++;
-                mask.push_back(outcome.length());
-                mask.push_back(length.at(length_index));
-                length_helper = length_helper + length.at(length_index);
-                if(length_helper == 0){
-                    mask.push_back(length.at(length_index) +100);
-                }
-                again = true;
-            }
-            again = false;
-            outcome = outcome + letter + std::to_string(apperance);
+    return C;
 }
 
+void encode2(std::vector<uint64_t> start, std::vector<int> &mask, std::string &outcome){
+    //std::string input = std::to_string(start.at(0));
+    std::string input = "";
+    std::string input2 = "";
+    for(int i=0;i<start.size();i++){
+        input = input + std::to_string(start.at(i));
+        input2 = input2 + std::to_string(start.at(i)) + 'A';
+    }
+    char helper;
+    int count = 0;
+    int step = 0;
+    int max_count = 0;
+    int test_count=0;
+    mask.push_back(0);
+    for(int i = 0;i<input.length();i++){
+        if(input2[step]== 'A'){
+            mask.push_back(max_count);
+            mask.push_back(test_count);
+        }
+        if(count == 0){
+            helper = input[i];
+            count = 1;
+        }
+        else{
+            if(helper == input[i]){
+                count++;
+            }
+            else{
+                if(count > 9){
+                    outcome = outcome + helper + 'A' + std::to_string(count);
+                }
+                else{
+                    outcome = outcome + helper + std::to_string(count);
+                }
+                helper = input[i];
+                count = 1;
+                test_count =0;
+            }
+        }
+        if(input2[step]== 'A'){
+            mask.push_back(outcome.length());
+            max_count = 0;
+            step++;
+            test_count=0;
+        }
+        max_count++;
+        step++;
+        test_count++;
+    }
+    mask.push_back(max_count);
+    mask.push_back(0);
 
+        if(count > 9){
+                    outcome = outcome + helper + 'B' + std::to_string(count);
+                }
+                else{
+                    outcome = outcome + helper + std::to_string(count);
+                }
+    
+}
 
 
 void generate2(){
     std::cout << "Werte: 7776, 66, 644, 4, 445, 648, 8822, 2"<< std::endl;
     std::cout << "input: 77766664444564888222"<< std::endl;
-    std::cout << "output: 7364445161418323" << std::endl;
+    std::cout << "output: 7364455161418323" << std::endl;
     std::cout << "Maske: 0,4,2,2,2,3,4,1,4,2,8,3,10,4,12,1"<< std::endl;
 }
 
 
-void generate(std::vector<int> &start){
-    for(int i=0;i<10;i++){
+void generate(std::vector<uint64_t> &start,int elementcount){
+    
+    for(int i=0;i<elementcount;i++){
         start.push_back(rand() % 1024);
     }
+    
+    /**
+    start.push_back(776);
+    start.push_back(66);
+    start.push_back(644);
+    start.push_back(4);
+    start.push_back(445);
+    start.push_back(648);
+    start.push_back(8822);
+    start.push_back(2);
+    **/
+
+}
+
+void validate(std::vector<uint64_t> h, int* d) {
+    for (size_t i = 0; i < h.size(); i++) {
+        if (h.at(i) != d[i]) {
+            std::cout << "found invalidated field in element " << i << std::endl;
+            std::cout << "on CPU side: " << h.at(i) << std::endl;
+            std::cout << "on GPU side: " << d[i] << std::endl;
+            
+            
+        }
+    }
+}
+
+char* to_char_array(std::string s){
+    char* answer = (char*)malloc(s.length() * sizeof(char));
+    for(int i=0;i<s.length();i++){
+        answer[i] = s[i];
+    }
+    return answer;
+}
+
+int* to_int_array(std::vector<int> v){
+    int* answer = (int*)malloc(v.size() * sizeof(int));
+    for(int i=0;i<v.size();i++){
+        answer[i] = v.at(i);
+    }
+    return answer;
 }
 
 
 int main()
-{/**
-     std::vector<int> start;
+{
+     std::vector<uint64_t> start;
      std::vector<int> mask;
     std::string outcome = "";
+
+    std::vector<uint64_t> start2;
+    std::vector<int> mask2;
+    std::string outcome2 = "";
+
+    int elementcount = 10;
     
-    generate(start);
-    for (auto i: start)
+    generate(start, elementcount);
+    generate(start2, elementcount);
+    //for (auto i: start)
+    //    std::cout << i << ", ";
+    //std::cout<<std::endl;
+    
+    encode2(start, mask, outcome);
+    encode2(start2, mask2, outcome2);
+    
+    std::cout << outcome << std::endl;
+    
+    for (auto i: mask)
+        std::cout << i << ", ";
+    std::cout<<std::endl;
+
+    std::cout << outcome2 << std::endl;
+
+    for (auto i: mask2)
         std::cout << i << ", ";
     std::cout<<std::endl;
     
-    encode(start, mask, outcome);
+
+    char* A;
+    char* d_A;
+    int* mask_A;
+    int* d_mask_A;
+    char* B;
+    char* d_B;
+    int* mask_B;
+    int* d_mask_B;
+
+    int* d_C;
+
+    int* h_out;
+    size_t bytes1 = outcome.length() * sizeof(char);
+    size_t bytes2 = mask.size() * sizeof(int);
+
+    h_out = (int*)malloc(elementcount*sizeof(int));
+
+    A = (char*)malloc(bytes1);
+    mask_A = (int*)malloc(bytes2);
+
+    B = (char*)malloc(bytes1);
+    mask_B = (int*)malloc(bytes2);
+
+    cudaMalloc(&d_A, bytes1);
+    cudaMalloc(&d_mask_A, bytes2);
+
+    cudaMalloc(&d_B, bytes1);
+    cudaMalloc(&d_mask_B, bytes2);
+
+    cudaMalloc(&d_C, elementcount* sizeof(int));
+
+    A = to_char_array(outcome);
+    B = to_char_array(outcome2);
+    mask_A = to_int_array(mask);
+    mask_B = to_int_array(mask2);
+
+
+    cudaMemcpy( d_A, A, bytes1, cudaMemcpyHostToDevice);
+    cudaMemcpy( d_mask_A, mask_A, bytes2, cudaMemcpyHostToDevice);
+    cudaMemcpy( d_B, B, bytes1, cudaMemcpyHostToDevice);
+    cudaMemcpy( d_mask_B, mask_B, bytes2, cudaMemcpyHostToDevice);
+    cudaMemset(d_C, 0, elementcount * sizeof(int));
+
+    add<<<64, 1024>>>(d_A,d_B,d_C, d_mask_A,d_mask_B , elementcount);
+    cudaError_t cudaerr = cudaDeviceSynchronize();
+    if (cudaerr != cudaSuccess)
+        printf("kernel launch failed with error \"%s\".\n",
+               cudaGetErrorString(cudaerr));
+
+    cudaMemcpy(h_out, d_C, elementcount* sizeof(int), cudaMemcpyDeviceToHost);
     
-    std::cout << outcome << std::endl;
-    for (auto i: mask)
-        std::cout << i << ", ";
-    **/
- 
-    int* A;
-    int* d_A;
-    int* mask;
-    int* d_mask;
-    size_t bytes = 6 * sizeof(int);
+    std::vector<uint64_t> cpu = compare_cpu(start, start2);
 
-    A = (int*)malloc(bytes);
-    mask = (int*)malloc(9 * sizeof(int));
-
-    cudaMalloc(&d_A, bytes);
-    cudaMalloc(&d_mask, 9 * sizeof(int));
-
-    A[0] = 7;
-    A[1] = 3;
-    A[2] = 6;
-    A[3] = 4;
-    A[4] = 4;
-    A[5] = 2;
-
-    mask[0] = 0;
-    mask[1] = 4;
-    mask[2] = 1;
-    mask[3] = 2;
-    mask[4] = 2;
-    mask[5] = 2;
-    mask[6] = 2;
-    mask[7] = 3;
-    mask[8] = 0;
-
-
-    cudaMemcpy( d_A, A, bytes, cudaMemcpyHostToDevice);
-    cudaMemcpy( d_mask, mask, 9 * sizeof(int), cudaMemcpyHostToDevice);
-
-    hello_world<<<1, 1>>>(d_A, d_mask, 0);
-    cudaDeviceReset();
-    
-    
-    
+    validate(cpu, h_out);
     
     
 }
